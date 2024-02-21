153c691f2f6d3645a3f49e2f466465e310aa154f
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-02-21 13:48:14 -0800

    object and array mapping playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import './App.css'
 
-const title = 'React';
+const welcome = {
+  title: 'Useful Game',
+  greeting: 'wasa-wasa'
+}
+const kids = ['Alan', 'Barry', 'Cameron', 'Daniella'];
 
 function App() {
   return (
     <div className="top-wrapper">
-      <div>Hello {title}</div>
+      <h1>
+        {welcome.title}: {welcome.greeting}
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <ul>
+        {kids.map(kid => <li>{kid}</li>)}
+      </ul>
     </div>
   )
 }
