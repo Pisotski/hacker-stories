6fe5b0f77cf15eb92225b48214b0a8e4649a491e
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-02-21 18:50:05 -0800

    implemented list in react

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,26 @@ const welcome = {
   title: 'Useful Game',
   greeting: 'wasa-wasa'
 }
-const kids = ['Alan', 'Barry', 'Cameron', 'Daniella'];
+
+// Go ahead and render the item’s url, author, num_comments, and points as well. 
+// In the special case of the url, use an HTML anchor HTML element (read: <a> tag) that surrounds the title. 
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, ];
 
 function App() {
   return (
@@ -15,9 +34,19 @@ function App() {
       </h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
-      <ul>
-        {kids.map(kid => <li>{kid}</li>)}
-      </ul>
+      <hr />
+      <div>
+        {list.map((item) => {
+          return (
+            <ul className="technology" key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+              <li>Author: {item.author}</li>
+              <li>Comments: {item.num_comments}</li>
+              <li>Points: {item.points}</li>
+            </ul>
+          )
+        })}
+      </div>
     </div>
   )
 }
