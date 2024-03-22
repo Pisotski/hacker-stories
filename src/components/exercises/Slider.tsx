3190ba004990b24e15e5ba3abf2a84ad93d4be22
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-03-22 12:03:19 -0700

    ts for slider

--- src/components/exercises/Slider.tsx
+++ src/components/exercises/Slider.tsx
@@ -1,48 +1,52 @@
 import "./Slider.css";
-import { useState, useEffect, useRef } from "react";
+import { useRef } from "react";
 
-const getPercentage = (current, max) => (100 * current) / max;
+const getPercentage = (current: number, max: number) => (100 * current) / max;
 
-const getLeft = (percentage) => `calc(${percentage}% - 5px)`;
+const getLeft = (percentage: number) => `calc(${percentage}% - 5px)`;
 
 const Slider = () => {
-	const thumbRef = useRef();
-	const sliderRef = useRef();
-	const diff = useRef();
+	const thumbRef = useRef<HTMLDivElement>(null);
+	const sliderRef = useRef<HTMLDivElement>(null);
+	const diff = useRef<number>();
 
-	const handleMouseMove = (event) => {
-		let newX =
-			event.clientX -
-			diff.current -
-			sliderRef.current.getBoundingClientRect().left;
+	const handleMouseMove = (event: MouseEvent) => {
+		if (diff.current && sliderRef.current && thumbRef.current) {
+			let newX =
+				event.clientX -
+				diff.current -
+				sliderRef.current.getBoundingClientRect().left;
 
-		const end = sliderRef.current.offsetWidth - thumbRef.current.offsetWidth;
+			const end = sliderRef.current.offsetWidth - thumbRef.current.offsetWidth;
 
-		const start = 0;
+			const start = 0;
 
-		if (newX < start) {
-			newX = 0;
+			if (newX < start) {
+				newX = 0;
+			}
+
+			if (newX > end) {
+				newX = end;
+			}
+
+			const newPercentage = getPercentage(newX, end);
+
+			thumbRef.current.style.left = getLeft(newPercentage);
 		}
-
-		if (newX > end) {
-			newX = end;
-		}
-
-		const newPercentage = getPercentage(newX, end);
-
-		thumbRef.current.style.left = getLeft(newPercentage);
 	};
 	const handleOnMouseUp = () => {
 		document.removeEventListener("mouseup", handleOnMouseUp);
 		document.removeEventListener("mousemove", handleMouseMove);
 	};
 
-	const handleOnMouseDown = (event) => {
-		diff.current =
-			event.clientX - thumbRef.current.getBoundingClientRect().left;
+	const handleOnMouseDown = (event: MouseEvent) => {
+		if (diff.current && thumbRef.current) {
+			diff.current =
+				event.clientX - thumbRef.current.getBoundingClientRect().left;
 
-		document.addEventListener("mousemove", handleMouseMove);
-		document.addEventListener("mouseup", handleOnMouseUp);
+			document.addEventListener("mousemove", handleMouseMove);
+			document.addEventListener("mouseup", handleOnMouseUp);
+		}
 	};
 
 	return (
